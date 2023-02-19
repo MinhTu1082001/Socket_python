a06e4626f79727b583c26b4865f5a1cab1f226b2
Redgold <72465223+tmtumk@users.noreply.github.com>
2023-02-20 01:18:08 +0700

    .

--- Parsing.py
+++ Parsing.py
@@ -6,10 +6,14 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
+url = input('Enter link')
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 
 tags = soup('a')
 for tag in tags:
     print(tag.get('href', None))
+    print('TAG:', tag)
+    print('URL:', tag.get('href', None))
+    print('Contents:', tag.contents[0])
+    print('Attrs:', tag.attrs)
